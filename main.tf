8f67102a26f323e1c2fb444cdba6fd4866425c4e
kiran1616 <kirankumar7163@gmail.com>
2023-06-06 16:45:00 +0530

    m

--- main.tf
+++ main.tf
@@ -154,12 +154,12 @@ output "vpc" {
 
 
 # creating EKS
-module "eks" {
-  source      = "github.com/r-devops/tf-module-eks"
-  ENV         = var.env
-  PRIVATE_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
-  PUBLIC_SUBNET_IDS   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "app", null), "subnet_ids", null)
-  DESIRED_SIZE        = 2
-  MAX_SIZE            = 2
-  MIN_SIZE            = 2
-}
+#module "eks" {
+#  source      = "github.com/r-devops/tf-module-eks"
+#  ENV         = var.env
+#  PRIVATE_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
+#  PUBLIC_SUBNET_IDS   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "app", null), "subnet_ids", null)
+#  DESIRED_SIZE        = 2
+#  MAX_SIZE            = 2
+#  MIN_SIZE            = 2
+#}
